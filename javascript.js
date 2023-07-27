49b0fce75b5d17b9f9281e2329941027fcd2621c
CristianNicolae <76454832+crissty21@users.noreply.github.com>
2023-07-27 15:40:40 +0300

    checkpoint

--- javascript.js
+++ javascript.js
@@ -157,3 +157,37 @@ document.addEventListener('wheel', (event) => {
         }
     }
 });
+
+var videoId = 'my-video';
+var scaleFactor = 0.25;
+var snapshots = [];
+
+
+function capture(video, scaleFactor) {
+    if (scaleFactor == null) {
+        scaleFactor = 1;
+    }
+    var w = video.videoWidth * scaleFactor;
+    var h = video.videoHeight * scaleFactor;
+    var canvas = document.createElement('canvas');
+    canvas.width = w;
+    canvas.height = h;
+    var ctx = canvas.getContext('2d');
+    ctx.drawImage(video, 0, 0, w, h);
+    return canvas;
+}
+
+
+function shoot() {
+    var video = player.el().querySelector('video');
+    var output = document.getElementById('output');
+    var canvas = capture(video, scaleFactor);
+    canvas.onclick = function () {
+        window.open(this.toDataURL(image / jpg));
+    };
+    snapshots.unshift(canvas);
+    output.innerHTML = '';
+    for (var i = 0; i < 4; i++) {
+        output.appendChild(snapshots[i]);
+    }
+}
